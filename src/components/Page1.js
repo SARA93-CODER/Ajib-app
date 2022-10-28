425dd6e2f17e717c095ab7771202ca965c4d6ab6
SARA93-CODER <sara.inbashi@hotmail.com>
2022-10-28 09:52:11 +0400

    editing designs

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,21 +1,19 @@
 import {
   Box,
   Center,
-  Flex,
   Grid,
   Heading,
   HStack,
   Image,
   Progress,
+  Stack,
   Text,
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
-import { FiPhoneCall } from "react-icons/fi";
-// import { Logo } from "../images/Logo";
-
-// import Logo from "../images/logo.jpg";
-// import Weather from "../images/weather.jpg";
+import Background from "../images/background.jpg";
+import Logo from "../images/logo.png";
+import Weather from "../images/weather.png";
 import {
   CashDeposit,
   CashDrawal,
@@ -29,71 +27,102 @@ import InternalTransfare from "./assets/icon/InternalTransfare";
 
 const Page1 = () => {
   return (
-    <VStack
-
-    // bgImage="url('/images/background.jpg')"
-    // bgPosition="center"
-    // bgRepeat="no-repeat"
+    // <Box>
+    <Stack
+      bgImage={Background}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize={"cover"}
+      height="full"
+      py={"2"}
     >
-      <HStack w="full" spacing="300px" py="2" px="5">
-        <Box h="100px" w="300px" bg="transparent" py="7">
-          {/* <Image src={Logo} /> */}
-          {/* <Logo /> */}
-        </Box>
-        <VStack h="100px" w="300px" bg="transparent" alignItems="center" py="2">
-          <Heading fontSize="x-large" color="yellow.600">
+      <HStack spacing="340px" px="5">
+        <Stack>
+          <Image src={Logo} h="16" w="72" />
+        </Stack>
+
+        <Stack h="80px" w="350px" bg="transparent" alignItems="center">
+          <Heading
+            fontSize="x-large"
+            color="yellow.500"
+            fontWeight={"semibold"}
+            mt="2"
+          >
             Welcome
           </Heading>
-          <Text>Mr. Name Surname</Text>
-        </VStack>
-        <Box h="100px" w="270px" bg="transparent">
-          <HStack>
-            <VStack>
-              <Box fontSize="sm">August 28,10.20</Box>
-              <Box fontWeight="semibold">24&deg; C</Box>
-            </VStack>
-            <Box>
-              {/* <Image src={Weather} /> */}
-            </Box>
+          <Box textColor="white">Mr. Name Surname</Box>
+        </Stack>
 
-            {/* <Weather /> */}
-          </HStack>
-        </Box>
+        <HStack h="80px" w="400px" bg="transparent">
+          <VStack>
+            <Text
+              fontSize="sm"
+              textColor="white"
+              // justifyContent={"end"}
+              // textAlign="end"
+              // justifyItems={"end"}
+            >
+              August 28,10.20
+            </Text>
+            <Text
+              fontWeight="semibold"
+              textColor="white"
+              // justifyContent={"end"}
+            >
+              24&deg; C
+            </Text>
+          </VStack>
+          <Image src={Weather} h="80px" w="30" />
+        </HStack>
       </HStack>
       <Progress
-        my="2"
-        colorScheme="yellow.500"
+        // mb="2"
+        colorScheme="yellow"
         size="sm"
         value={50}
         background="blue.500"
       />
-      <Box spacing={6} pl="44" w="100%" mb="10">
-        <Heading as="h1" size="lg" color="white" fontFamily="sans-serif" my="8">
+      <Box pl="60">
+        <Heading
+          as="h1"
+          size="lg"
+          color="white"
+          fontFamily="sans-serif"
+          py={"6"}
+          fontWeight="normal"
+        >
           Please select a Transaction
         </Heading>
         <Box>
-          <Grid spacing="0" templateColumns="repeat(4, 0fr)" w="full">
+          <Grid spacing="0" templateColumns="repeat(4, 0fr)">
             <Center
-              w="80"
-              h="44"
+              w="72"
+              h="40"
               bg="white"
-              p="6"
+              // p="6"
               roundedTopLeft="md"
-              bgColor={"yellow.500"}
-              opacity="75%"
+              opacity="80%"
+              bgGradient="linear(to-t,yellow.500 , yellow.300 )"
+              bgColor={"yellow.500/80"}
               borderWidth="thin"
               borderColor={"blue.600"}
             >
               <HStack>
-                <Box>
-                  <CashDrawal />
-                </Box>
+                {/* <Box
+                  border={"white"}
+                  borderRadius="3xl"
+                  rounded="full"
+                  bg={"yellow.500"}
+                  h="80px"
+                  w="80px"
+                > */}
+                <CashDrawal />
+                {/* </Box> */}
                 <VStack
                   textColor={"white"}
                   fontWeight="normal"
                   fontSize={"x-large"}
                   spacing="0.5"
-                  justify={"start"}
                 >
                   <Box>Cash </Box>
                   <Box>Withdrawal</Box>
@@ -102,8 +131,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-b, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -117,8 +146,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-b, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -132,8 +161,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-b, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -147,13 +176,13 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="80"
-              h="44"
+              w="72"
+              h="40"
               bg="white"
               p="6"
               roundedBottomLeft="md"
               bgColor={"yellow.500"}
-              opacity="75%"
+              opacity="80%"
               borderWidth="thin"
               borderColor={"blue.600"}
             >
@@ -169,8 +198,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-t, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -184,8 +213,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-t, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -199,8 +228,8 @@ const Page1 = () => {
             </Center>
 
             <Center
-              w="64"
-              h="44"
+              w="52"
+              h="40"
               bgGradient="linear(to-t, blue.100, blue.50)"
               borderWidth="thin"
               borderColor={"blue.600"}
@@ -215,17 +244,18 @@ const Page1 = () => {
           </Grid>
         </Box>
       </Box>
-      {/* Footer : */}
-      <Flex color="white" gap="6">
-        <Box>
-          <FiPhoneCall />
-          <Text>Contact Center</Text>
-        </Box>
-        <Box>
-          <Text>Website: </Text>
-        </Box>
-      </Flex>
-    </VStack>
+
+      {/* <Box color="white" gap="6">
+          <Box>
+            <FiPhoneCall />
+            <Text>Contact Center</Text>
+          </Box>
+          <Box>
+            <Text>Website: </Text>
+          </Box>
+        </Box> */}
+    </Stack>
+    // </Box>
   );
 };
 
